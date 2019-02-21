c2d08c6fce379a62df505f899ead6f7635ea202c
Valery Vargin <vdvugad@gmail.com>
2019-02-21 23:06:44 +0300

    fix "MobX provider store changed"

--- src/Provider/EntitiesProvider.jsx
+++ src/Provider/EntitiesProvider.jsx
@@ -2,6 +2,8 @@ import { Provider } from 'mobx-react'
 import React from 'react'
 import RootStore from '../Store/RootStore'
 
+const rootStore = new RootStore()
+
 export default function EntitiesProvider ({children}) {
-  return <Provider store={new RootStore()}>{children}</Provider>
+  return <Provider store={rootStore}>{children}</Provider>
 }
