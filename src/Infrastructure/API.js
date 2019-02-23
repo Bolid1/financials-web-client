7ed58394fe71f47b783c0f5e28e8787becf39d71
Valery Vargin <vdvugad@gmail.com>
2019-02-23 20:31:56 +0300

    Получение конкретной облигации из API

--- src/Infrastructure/API.js
+++ src/Infrastructure/API.js
@@ -1,21 +1,37 @@
-import bond from '../samples/bond'
 import bonds from '../samples/bonds'
 import currencies from '../samples/currencies'
 import issuers from '../samples/issuers'
 
 const entityMap = {
-  bond,
   bonds,
   issuers,
   currencies,
+}
+
+const singleToMultiple = {
+  bond: 'bonds',
+}
+
+const identifiers = {
+  bond: 'ISIN',
 }
 
 export default class API {
   static find (entity, id) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        if (entityMap.hasOwnProperty(entity)) {
-          return resolve(JSON.parse(entityMap[entity]))
+        const multiple = singleToMultiple[entity]
+        const identifier = identifiers[entity]
+
+        if (identifier && multiple && entityMap.hasOwnProperty(multiple)) {
+          const data = JSON.parse(entityMap[multiple])
+          const entities = data._embedded[multiple]
+          delete data._embedded[multiple]
+          const result = (entities || []).find(entity => entity[identifier] === id)
+
+          if (result) {
+            return resolve(Object.assign(result, data))
+          }
         }
 
         return reject('Not found')
